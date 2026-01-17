cd48460594b2dd990836474f370268237d50c08a
HaithemOfficial <haithemofficial@users.noreply.github.com>
2026-01-16 23:33:02 -0100

    Remove all code outside tests/ that is not needed for running tests

--- tests/test_e2e_employee.py
+++ tests/test_e2e_employee.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from selenium.common.exceptions import TimeoutException
 
 
 class TestOrangeHRME2E:
